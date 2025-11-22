c1bf53c266b297860acfd490e9bc610410466d7e
zhujiatao <1975841338@qq.com>
2025-11-22 22:55:23 +0800

    [refactor] 升级 STM32CubeMX 版本至 6.16.0
    [chore] 删除 DFSDM 未使用的引脚配置

--- STM32CubeMX/Target_1/MX_Device/MX_Device.h
+++ STM32CubeMX/Target_1/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 18/11/2025 16:58:19
+ * Date        : 22/11/2025 22:54:09
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
