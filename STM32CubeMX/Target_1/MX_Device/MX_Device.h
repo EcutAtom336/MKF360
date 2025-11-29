788e199893508fac63f504a80bc10642b374d919
zhujiatao <1975841338@qq.com>
2025-11-29 15:09:46 +0800

    [fix] 修复 DAC/ADC 触发 TIM 频率错误

--- STM32CubeMX/Target_1/MX_Device/MX_Device.h
+++ STM32CubeMX/Target_1/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 24/11/2025 18:26:55
+ * Date        : 29/11/2025 15:09:24
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
